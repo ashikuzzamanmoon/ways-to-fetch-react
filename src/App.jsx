2c9aa844c82f835faeb95f805bdc1c24b0654b6c
Ashikuzzaman_Moon <ashikuzzamanmoon31@gmail.com>
2024-09-08 19:14:33 +0600

    use fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import "./App.css";
 import Quote from "./components/query/Quote";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
+  const [quote, setQuote] = useState(null);
+
+  useEffect(() => {
+    const fetchQuote = async () => {
+      const res = await fetch("https://api.quotable.io/random");
+      const data = await res.json();
+      setQuote(data);
+    };
+
+    fetchQuote();
+  }, []);
+
   return (
     <>
       <div>
-        <Quote />
+        <Quote quote={quote} />
+        <Quote quote={quote} />
       </div>
     </>
   );
--- src/components/query/Quote.jsx
+++ src/components/query/Quote.jsx
@@ -1,18 +1,4 @@
-import { useEffect } from "react";
-import { useState } from "react";
-
-const Quote = () => {
-  const [quote, setQuote] = useState(null);
-
-  useEffect(() => {
-    const fetchQuote = async () => {
-      const res = await fetch("https://api.quotable.io/random");
-      const data = await res.json();
-      setQuote(data);
-    };
-
-    fetchQuote();
-  }, []);
+const Quote = ({ quote }) => {
   return (
     <div>
       <h1>Get Quotes using fetch API</h1>
